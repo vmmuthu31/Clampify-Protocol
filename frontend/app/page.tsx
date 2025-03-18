5b056a680d7b9cb190845d5a63268c8e7d4f2223
Vairamuthu <mvairamuthu2003@gmail.com>
2025-03-18 14:34:27 +0530

    refactor: remove unused frontend and contract files to streamline the project structure

--- frontend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Clampify</h1>
-    </div>
-  );
-}
