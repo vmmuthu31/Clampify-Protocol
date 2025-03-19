629c2747a9a7ffef145dd430ce5ab32bda849c67
Vairamuthu <mvairamuthu2003@gmail.com>
2025-03-19 13:06:31 +0530

    feat: add Footer and Navbar components to ClientBody for improved layout

--- frontend/app/ClientBody.tsx
+++ frontend/app/ClientBody.tsx
@@ -2,10 +2,15 @@
 
 import { ReactNode } from "react";
 import WalletButtonProvider from "@/providers/WalletButtonProvider";
+import Footer from "@/components/Footer";
+import { Navbar } from "@/components/navbar";
+
 function ClientBody({ children }: { children: ReactNode }) {
   return (
     <div>
+      <Navbar />
       <WalletButtonProvider>{children}</WalletButtonProvider>
+      <Footer />
     </div>
   );
 }
